868f8850b1e5e74f0fff6e428f468c8393fdc634
shinm3 <remiriasine@yahoo.co.jp>
2021-02-06 19:16:12 +0900

    character/views.py, discipline/views.py を修正

--- character/views.py
+++ character/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render, redirect
-from django.core.files import File
 from .models import Character
 from .forms import CharacterForm
-from django.conf import settings
-import os
 # Create your views here.
 
 
@@ -21,17 +18,17 @@ def character_selection(request):
     if request.method == 'POST':
         form = CharacterForm(request.POST)
         if form.is_valid():
-            # バリデーション成功後、削除するためのキャラクターオブジェクトを取得
+            # バリデーション成功後、削除するための現在設定中のキャラクターオブジェクトを取得
             del_objects = Character.objects.filter(user=user)
             if del_objects:
-                # キャラクターオブジェクトがあればそれを削除
+                # 現在設定中のキャラクターオブジェクトがあればそれを削除
                 for del_object in del_objects:
                     del_object.delete()
             character = form.save(commit=False)
             character.user = user
             name = request.POST.get('name')
             character.name = name
-            number = request.POST.get('character')
+            number = request.POST.get('character')  # キャラクターを表す番号
             character.number = number
 
             character.save()
--- discipline/views.py
+++ discipline/views.py
@@ -11,8 +11,8 @@ def discipline_create(request, pk, year, month, day, new):
     """ 新規に指定部位の種目を作成します。 """
 
     date = datetime.date(year=year, month=month, day=day)
-    body_part = get_object_or_404(BodyPart, pk=pk)
-    discipline_set = body_part.discipline_set.filter(date=date)
+    body_part = get_object_or_404(BodyPart, pk=pk)  # 指定した部位のオブジェクト取得
+    discipline_set = body_part.discipline_set.filter(date=date)  # 指定した日付に設定してあるオブジェクトのセット取得
     # ページタイトルの作成
     page_title = body_part.page_title()
 
@@ -23,7 +23,7 @@ def discipline_create(request, pk, year, month, day, new):
             discipline.date = date
             discipline.body_part = body_part
             discipline.save()
-            if not request.POST.get('continue'):
+            if not request.POST.get('continue'):  # 続けて種目登録する場合の処理
                 return redirect('discipline:day_schedule_discipline',
                                 pk=pk, year=year, month=month, day=day)
             else:
@@ -49,8 +49,8 @@ def day_schedule_discipline(request, pk, year, month, day):
     """ 指定部位の種目一覧を表示させます。 """
 
     date = datetime.date(year=year, month=month, day=day)
-    body_part = get_object_or_404(BodyPart, pk=pk)
-    discipline_set = body_part.discipline_set.filter(date=date)
+    body_part = get_object_or_404(BodyPart, pk=pk)  # 指定した部位のオブジェクト取得
+    discipline_set = body_part.discipline_set.filter(date=date)  # 指定した日付に設定してあるオブジェクトのセット取得
 
     # ページタイトルの作成
     page_title = body_part.page_title()
@@ -71,7 +71,7 @@ def day_schedule_discipline(request, pk, year, month, day):
 def discipline_update(request, pk, year, month, day):
     """ 指定部位の種目を更新します。 """
 
-    new = 0
+    new = 0  # すでに指定部位の種目が一つでも設定されてあれば０、されてなければ1となります。更新なので既に設定されてるため0になります
     date = datetime.date(year=year, month=month, day=day)
     discipline = get_object_or_404(Discipline, pk=pk)
     body_part_pk = discipline.body_part.pk
@@ -104,20 +104,22 @@ def discipline_update(request, pk, year, month, day):
 @require_POST
 def discipline_delete(request, year, month, day):
     """ 指定部位の種目を削除します。 """
-    del_pk = request.POST.getlist('delete')
-
-    pk = del_pk[0]
-    discipline = get_object_or_404(Discipline, pk=pk)
+    del_pk_list = request.POST.getlist('delete')  # 削除する種目のpkリスト
+    # 削除する前に種目設定してある先の部位のpkを取得
+    del_pk = del_pk_list[0]
+    discipline = get_object_or_404(Discipline, pk=del_pk)
     body_part_pk = discipline.body_part.pk
-
-    del_objects = Discipline.objects.filter(pk__in=del_pk)
+    # 選んだ種目を削除
+    del_objects = Discipline.objects.filter(pk__in=del_pk_list)
     del_objects.delete()
 
-    new = 1
+    new = 1  # 全て削除されれば設定されている種目がなくなるので１になります。
+
+    # 削除後、削除されていない種目のオブジェクトを取得
     date = datetime.date(year=year, month=month, day=day)
     body_part = get_object_or_404(BodyPart, pk=body_part_pk)
     discipline_set = body_part.discipline_set.filter(date=date)
-    if discipline_set:
+    if discipline_set:  # 削除されていない種目があれば既に設定されている種目があるので「new」を０にします
         new = 0
 
     return redirect('discipline:discipline_create', pk=body_part_pk, year=year, month=month, day=day, new=new)
